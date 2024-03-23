b978ee8f1e3c1beee5815adeaaf4318147533183
BradleyLikesCoding <112587989+BradleyLikesCoding@users.noreply.github.com>
2024-03-23 15:40:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
 const config = {
-  challenge: true, // Set to true if you want to enable password protection.
+  challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    bradley: 'BRADley123!',
-    ethan: '132435',
-    flynn: '1l0v3FGCC',
-    logan: 'cappello',
-    shyaan: '13431346'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
